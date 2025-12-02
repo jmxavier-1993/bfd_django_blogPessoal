60c62c21d4a6243d53d762b8475d253d06abdf53
Juliana Xavier <juliana.marinhoppf5@hotmail.com>
2025-12-02 20:58:48 -0300

    update

--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,4 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.all()
-    
-    context = {
-        'posts': posts
-    }
-    
-    return render(request, 'blog/post_list.html', context)
+    return render(request, 'blog/post_list.html', {'posts': posts})
--- site_pessoal/urls.py
+++ site_pessoal/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.urls import path, include 
 from django.contrib import admin
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
