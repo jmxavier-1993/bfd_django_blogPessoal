f0a732fab3371e1a25238f8ceb40493b0cfc0469
jmxavier-1993 <juliana.marinhoppf5@hotmail.com>
2025-12-22 23:57:56 -0300

    update

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.models import User
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
 
 
 class Post(models.Model):
@@ -16,3 +18,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=15, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics', null=True, blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+    data_nascimento = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.username
--- site_pessoal/urls.py
+++ site_pessoal/urls.py
@@ -17,8 +17,15 @@ Including another URLconf
 from django.urls import path, include 
 from django.contrib import admin
 from blog import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')), 
 ]
+
+# Isso permite servir arquivos de mídia durante o desenvolvimento
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
