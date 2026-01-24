cc6a98b5439164fdf9aa5f76639a14965a2d37f2
jmxavier-1993 <juliana.marinhoppf5@hotmail.com>
2026-01-24 14:18:02 -0300

    update conflitos2

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
-If the CRESTE_SUPERUSER environment variable is True, it create a superuser.
+# If the CRESTE_SUPERUSER environment variable is True, it create a superuser.
 if [[ $CREATE_SUPERUSER ]];
 then
   python manage.py createsuperuser --no-input
